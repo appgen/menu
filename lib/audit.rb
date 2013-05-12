f0cea666977919d77dd7d7fc58a88846493bf21f
ashley williams <heyashleyashley@gmail.com>
2013-05-12 13:23:37 -0400

    list offending depts and # of offending datsets

--- lib/audit.rb
+++ lib/audit.rb
@@ -10,10 +10,25 @@ class Audit
     @json_obj = MultiJson.decode(json_txt)
   end
 
+  def departments
+    depts = Hash.new
+    @json_obj.each do |superset|
+      superset["datasets"].each do |set|
+        if depts.include?(set["attribution"])
+          depts[set["attribution"]] = depts[set["attribution"]] + 1
+        else
+          depts[set["attribution"]] = 1
+        end
+      end
+    end
+    return depts
+  end
+
   private
 
   def self.load_file
     filename = File.join('data', "unionable.json")
     File.open(filename, 'r').read
   end
+
 end
