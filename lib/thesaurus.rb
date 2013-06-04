6e099b87d25ed0405d4beef35a3b4ef5ca33204c
Thomas Levine <occurrence@thomaslevine.com>
2013-06-04 15:15:03 -0700

    remove bacon ipsum

--- lib/thesaurus.rb
+++ lib/thesaurus.rb
@@ -5,14 +5,4 @@ class Thesaurus
 	CONTACT = ["contact your congressman", "contact", "take action", "get in touch", "act now!", "act", "communicate", "contact your rep"]
 	CARE = ["have interest in", "care about", "have concern for"]
 
-	IPSUM_BASEURL = "http://baconipsum.com/api/?="
-
-	def self.getParagraphs(num)
-		json_txt = HTTParty.get(IPSUM_BASEURL,{:query=> {:type=>"meat-and-filler", :paras=>num}})[0]
-	end
-
-	def self.getSentences(num)
-		json_txt = HTTParty.get(IPSUM_BASEURL,{:query=> {:type=>"meat-and-filler", :sentences=>num}})[0]
-	end
-
 end
